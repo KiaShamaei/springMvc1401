57f8d8799d31b2b0c7e34c870ee47c9bff8092d9
Shamaei.Kiarash <kiarash.shamaii@gmail.com>
2022-06-01 19:36:10 +0430

    add validation annotation

--- src/main/java/com/springmvc/controller/CustomerController.java
+++ src/main/java/com/springmvc/controller/CustomerController.java
@@ -2,9 +2,12 @@ package com.springmvc.controller;
 
 
 import com.springmvc.model.Customer;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,6 +16,13 @@ import javax.validation.Valid;
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+        dataBinder.registerCustomEditor(String.class , stringTrimmerEditor);
+    }
+
     @RequestMapping("")
     public String showCustomerForm (Model theModel){
         theModel.addAttribute("customer" , new Customer());
--- src/main/java/com/springmvc/model/Customer.java
+++ src/main/java/com/springmvc/model/Customer.java
@@ -1,16 +1,39 @@
 package com.springmvc.model;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+import org.springframework.web.bind.annotation.InitBinder;
+
+import javax.validation.constraints.*;
 
 public class Customer {
-    @NotEmpty(message = "is required")
-//    @Size(min=1)
+    @NotNull(message = "is required")
+    @Size(min=1, message = "is required")
     private String firstName ;
 
 
     private String lastName ;
+
+    @Max(value = 10 , message = "not larger than 10")
+    @Min(value = 5, message = "not less than 5")
+    @NotNull(message = "is required")
+    private Integer freePasses ;
+    @Pattern(regexp = "^[a-zA-z0-9]{7}" , message = "only 7 digit/chars ")
+    private  String postalCode ;
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public Integer getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(Integer freePasses) {
+        this.freePasses = freePasses;
+    }
 
     public String getFirstName() {
         return firstName;
